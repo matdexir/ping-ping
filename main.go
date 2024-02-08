0bf6a36109a1c372b37338bd2a9abcb83166c382
matdexir <matthieudesir21@gmail.com>
2024-02-08 17:24:26 +0800

    feat: added a simple server with echo

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	// "errors"
-	// "fmt"
-	"github.com/go-playground/validator/v10"
+	"net/http"
 	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
 )
 
 type Gender string
@@ -58,6 +59,31 @@ func (sp *SponsoredPost) Validate() error {
 	return validate.Struct(sp)
 }
 
+type QueryItems struct {
+	Title string    `json:"title" validate:"required"`
+	EndAt time.Time `json:"endAt" validate:"required"`
+}
+
+// func getSponsoredPost(c echo.Context) error {
+
+// 	offset := c.QueryParam("offset")
+// 	limit := c.QueryParam("limit")
+// 	age := c.QueryParam("age")
+// 	gender := c.QueryParam("gender")
+// 	country := c.QueryParam("country")
+// 	platform := c.QueryParam("platform")
+
+// 	return c.String(http.StatusOK, "")
+// }
+
+// func createSponsoredPost(c echo.Context) error {
+// 	return c.String(http.StatusOK, "")
+// }
+
 func main() {
-	println("hello world")
+	e := echo.New()
+	e.GET("/api/v1/ad", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, world.")
+	})
+	e.Logger.Fatal(e.Start(":4321"))
 }
