e22e55ef70395a95f789ea834b3585b6118e4ef1
matdexir <matthieudesir21@gmail.com>
2024-02-13 17:39:39 +0800

    feat: added support for marshalling and unmarshalling

--- models/reflect.go
+++ models/reflect.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 )
@@ -14,6 +15,23 @@ const (
 
 func (g Gender) String() string {
 	return [...]string{"Male", "Female"}[g]
+}
+
+func (g Gender) MarshalJSON() ([]byte, error) {
+	return json.Marshal(g.String())
+}
+
+func (g *Gender) UnmarshalJSON(data []byte) error {
+	var genderStr string
+	if err := json.Unmarshal(data, &genderStr); err != nil {
+		return err
+	}
+	parsed, ok := Parse[Gender](genderStr, GenderHint)
+	if !ok {
+		return fmt.Errorf("Unable to unmarshal Gender JSON")
+	}
+	*g = parsed.(Gender)
+	return nil
 }
 
 type Country int
@@ -31,6 +49,23 @@ func (c Country) String() string {
 	return [...]string{"Japan", "Taiwan", "USA", "Brazil", "SouthAfrica", "France"}[c]
 }
 
+func (c Country) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.String())
+}
+
+func (c *Country) UnmarshalJSON(data []byte) error {
+	var countryStr string
+	if err := json.Unmarshal(data, &countryStr); err != nil {
+		return err
+	}
+	parsed, ok := Parse[Country](countryStr, CountryHint)
+	if !ok {
+		return fmt.Errorf("Unable to unmarshal Country JSON")
+	}
+	*c = parsed.(Country)
+	return nil
+}
+
 type Platform int
 
 const (
@@ -41,6 +76,28 @@ const (
 
 func (p Platform) String() string {
 	return [...]string{"android", "iOS", "web"}[p]
+}
+
+func (p Platform) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.String())
+}
+
+func (p *Platform) UnmarshalJSON(data []byte) error {
+	var platformStr string
+
+	if err := json.Unmarshal(data, &platformStr); err != nil {
+		return err
+	}
+
+	parsed, ok := Parse[Platform](platformStr, PlatformHint)
+
+	if !ok {
+		return fmt.Errorf("Unable to parse the Platform json.")
+	}
+
+	*p = parsed.(Platform)
+
+	return nil
 }
 
 type SerializableItem interface {
@@ -86,11 +143,11 @@ func getMap(hint Hint) map[string]SerializableItem {
 // should I use generic?
 // SerializableItem needs to be a concrete type
 // however I am not too sure how to make it fit the requirement of both String()
-func Parse[T SerializableItem](item string, hint Hint) (T, bool) {
+func Parse[T SerializableItem](item string, hint Hint) (interface{}, bool) {
 	mp := getMap(hint)
 	value, ok := mp[strings.ToLower(item)]
 	if !ok {
-		return T{}, false
+		return nil, false
 	}
 	return value.(T), true
 }
@@ -115,7 +172,7 @@ func Deserialize[T SerializableItem](dbField string, hint Hint) ([]T, error) {
 		if !ok {
 			return nil, fmt.Errorf("unable to deserialize field: %s", split)
 		}
-		enums = append(enums, parsed)
+		enums = append(enums, parsed.(T))
 	}
 	return enums, nil
 }
@@ -123,7 +180,9 @@ func Deserialize[T SerializableItem](dbField string, hint Hint) ([]T, error) {
 func main() {
 	platforms := []Platform{ANDROID, IOS, WEB}
 	serializedPlatforms := Serialize(platforms)
+	marshaledPlatforms, _ := json.Marshal(platforms)
 	fmt.Println("Serialized platforms:", serializedPlatforms) // Output: android, iOS, web
+	fmt.Println("Marshaled platforms:", string(marshaledPlatforms))
 
 	genders := []Gender{Male, Female}
 	serializedGenders := Serialize(genders)
@@ -132,6 +191,13 @@ func main() {
 	gender, ok := Parse[Gender]("M", GenderHint)
 	if ok {
 		fmt.Println("Parsed gender:", gender) // Output: Male
+	} else {
+		fmt.Println("Error parsing gender")
+	}
+
+	country, ok := Parse[Country]("Japan", CountryHint)
+	if ok {
+		fmt.Println("Parsed country:", country) // Output: Japan
 	} else {
 		fmt.Println("Error parsing gender")
 	}
